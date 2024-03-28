f177510700069931cc45ff0fd3234b8aa8548a68
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-03-28 13:24:37 +0530

    Added opening and closing of Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,12 +3,12 @@ import "./Header.module.css";
 import classes from "./Header.module.css";
 import HeaderCartBtn from "./HeaderCartBtn";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartBtn />
+        <HeaderCartBtn onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src="https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
